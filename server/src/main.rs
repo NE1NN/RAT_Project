5f2d63accd8ac743925d6d076a1f4262afcbd705
Hindie <hindiesuputra@gmail.com>
2024-09-17 12:47:41 +1000

    refactor into a function

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,11 +1,29 @@
-use std::net::TcpListener;
+use std::{
+    io::{Error, Read},
+    net::{TcpListener, TcpStream},
+};
 
 fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
 
     for stream in listener.incoming() {
-        println!("hello world")
+        process_stream(stream);
     }
 
     Ok(())
+}
+
+fn process_stream(stream: Result<TcpStream, Error>) {
+    let mut stream = stream.unwrap();
+    let mut buffer = [0; 0124];
+
+    match stream.read(&mut buffer) {
+        Ok(_) => {
+            let message = String::from_utf8_lossy(&buffer[..]);
+            println!("Received: {}", message)
+        }
+        Err(e) => {
+            println!("Failed to read from stream: {}", e);
+        }
+    }
 }
