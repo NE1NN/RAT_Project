592c4f1478a6dda700288d9841b0681d706917f2
Hindie <hindiesuputra@gmail.com>
2024-09-17 17:42:32 +1000

    add execute command

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,20 +1,30 @@
 use std::{
-    io::{Error, Read},
+    io::{self, Error, Read},
     net::{TcpListener, TcpStream},
+    process::Command,
 };
 
 fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
 
     for stream in listener.incoming() {
-        process_stream(stream);
+        match stream {
+            Ok(_) => {
+                if let Err(e) = execute_command() {
+                    eprintln!("Failed to process {}", e)
+                }
+            }
+            Err(e) => {
+                eprintln!("Failed to process {}", e)
+            }
+        }
     }
 
     Ok(())
 }
 
-fn process_stream(stream: Result<TcpStream, Error>) {
-    let mut stream = stream.unwrap();
+fn process_stream(stream: Result<TcpStream, Error>) -> io::Result<()> {
+    let mut stream = stream?;
     let mut buffer = [0; 1204];
 
     match stream.read(&mut buffer) {
@@ -26,4 +36,19 @@ fn process_stream(stream: Result<TcpStream, Error>) {
             println!("Failed to read from stream: {}", e);
         }
     }
+    Ok(())
+}
+
+fn execute_command() -> io::Result<()> {
+    let output = if cfg!(target_os = "windows") {
+        Command::new("cmd").args(&["/C", "dir"]).output()?
+    } else {
+        Command::new("ls").output()?
+    };
+
+    println!(
+        "Command output: {}",
+        String::from_utf8_lossy(&output.stdout)
+    );
+    Ok(())
 }
